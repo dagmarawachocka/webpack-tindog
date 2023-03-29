d6f5c4c5dc41ba2ef1a2139c14f238260420cc34
Dagmara Wąchocka <dwachocka@gmail.com>
2023-03-29 14:37:34 +0200

    Add-Webpack-configuration

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log("Hello webpack!");
+import "./style/index-style.css";
+import "./assets/bizinsider.png";
+import "./assets/dog-img.jpg";
+import "./assets/google-play-brands.svg";
+import "./assets/iphone6.png";
+import "./assets/lady-img.jpg";
+import "./assets/mashable.png";
+import "./assets/TechCrunch.png";
+import "./assets/tindog.png";
+import "./assets/tnw.png";
+
---
+++ src/tindog-form.js
@@ -0,0 +1,3 @@
+import "./style/form-style.css";
+import "./js/form.ts";
+import "./assets/tindog.png";
